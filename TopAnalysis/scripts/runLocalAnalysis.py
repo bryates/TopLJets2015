e4dc7ba8a560b41c53708d912ae23b2808025627
Pedro Silva <pfs@users.noreply.github.com>
2015-10-08 13:26:34 +0200

    Update runLocalAnalysis.py

--- TopAnalysis/scripts/runLocalAnalysis.py
+++ TopAnalysis/scripts/runLocalAnalysis.py
@@ -11,7 +11,11 @@ def main():
     parser.add_option('-i', '--in',       dest='input',       help='input directory with files or single file',  default=None,       type='string')
     parser.add_option('-o', '--out',      dest='outDir',      help='output directory',                           default='analysis', type='string')
     parser.add_option(      '--ch',       dest='channel',     help='channel',                                    default=13,         type=int)
-    parser.add_option(      '--charge',   dest='charge',      help='charge',                                     default=0,         type=int)
+    parser.add_option(      '--charge',   dest='charge',      help='charge',                                     default=0,          type=int)
+    parser.add_option(      '--flav',     dest='flav',        help='flavour splitting (for single files)',       default=-1,         type=int)
+    parser.add_option(      '--isTT',     dest='isTT',        help='ttbar sample (for single files)',            action='store_true')
+   parser.add_option(       '--norm',     dest='norm',        help='normalization scale (for single files)',     default=1.0,         type=float)
+
     (opt, args) = parser.parse_args()
 
     ROOT.AutoLibraryLoader.enable()
@@ -20,7 +24,7 @@ def main():
     from ROOT import ReadTree
     print ReadTree
     if '.root' in opt.input:
-        ReadTree(opt.input,opt.outDir,opt.channel,opt.charge,1.0,False)
+        ReadTree(opt.input,opt.outDir,opt.channel,opt.charge,opt.norm,opt.isTT,opt.flav)
         
 
 """
