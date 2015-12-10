17061fbc1fea020b25b3e0fdf86cf0626aaf507a
Pedro Silva <pfs@users.noreply.github.com>
2015-12-10 10:29:31 +0100

    Update MiniAnalyzer.cc

--- TopAnalysis/plugins/MiniAnalyzer.cc
+++ TopAnalysis/plugins/MiniAnalyzer.cc
@@ -91,7 +91,10 @@ private:
   virtual void beginJob() override;
   virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
   virtual void endJob() override;
-
+  float getMiniIsolation(edm::Handle<pat::PackedCandidateCollection> pfcands,
+			 const reco::Candidate* ptcl,  
+                         float r_iso_min, float r_iso_max, float kt_scale,
+                         bool charged_only);
 
   // member data 
   edm::EDGetTokenT<edm::TriggerResults> triggerBits_;
@@ -646,6 +649,76 @@ MiniAnalyzer::endRun(const edm::Run & iRun, edm::EventSetup const & iSetup)
   catch(...){
     std::cout << "Failed to retrieve LHERunInfoProduct" << std::endl;
   }
+}
+
+//-------------
+//cf. https://twiki.cern.ch/twiki/bin/view/CMS/MiniIsolationSUSY
+float MiniAnalyzer::getMiniIsolation(edm::Handle<pat::PackedCandidateCollection> pfcands,
+			const reco::Candidate* ptcl,  
+                        float r_iso_min, float r_iso_max, float kt_scale,
+                        bool charged_only) 
+{
+
+    if (ptcl->pt()<5.) return 99999.;
+
+    float deadcone_nh(0.), deadcone_ch(0.), deadcone_ph(0.), deadcone_pu(0.);
+    if(ptcl->isElectron()) {
+      if (fabs(ptcl->eta())>1.479) {deadcone_ch = 0.015; deadcone_pu = 0.015; deadcone_ph = 0.08;}
+    } else if(ptcl->isMuon()) {
+      deadcone_ch = 0.0001; deadcone_pu = 0.01; deadcone_ph = 0.01;deadcone_nh = 0.01;  
+    } else {
+      //deadcone_ch = 0.0001; deadcone_pu = 0.01; deadcone_ph = 0.01;deadcone_nh = 0.01; // maybe use muon cones??
+    }
+
+    float iso_nh(0.), iso_ch(0.), iso_ph(0.) iso_pu(0.);
+    float ptThresh(0.5);
+    if(ptcl->isElectron()) ptThresh = 0;
+    float r_iso = max(r_iso_min,min(r_iso_max, kt_scale/ptcl->pt()));
+    for (const pat::PackedCandidate &pfc : *pfcands) {
+      if (abs(pfc.pdgId())<7) continue;
+
+      float dr = deltaR(pfc, *ptcl);
+      if (dr > r_iso) continue;
+      
+      //////////////////  NEUTRALS  /////////////////////////
+      if (pfc.charge()==0){
+        if (pfc.pt()>ptThresh) {
+          /////////// PHOTONS ////////////
+          if (abs(pfc.pdgId())==22) {
+            if(dr < deadcone_ph) continue;
+            iso_ph += pfc.pt();
+	    /////////// NEUTRAL HADRONS ////////////
+          } else if (abs(pfc.pdgId())==130) {
+            if(dr < deadcone_nh) continue;
+            iso_nh += pfc.pt();
+          }
+        }
+        //////////////////  CHARGED from PV  /////////////////////////
+      } else if (pfc.fromPV()>1){
+        if (abs(pfc.pdgId())==211) {
+          if(dr < deadcone_ch) continue;
+          iso_ch += pfc.pt();
+        }
+        //////////////////  CHARGED from PU  /////////////////////////
+      } else {
+        if (pfc.pt()>ptThresh){
+          if(dr < deadcone_pu) continue;
+          iso_pu += pfc.pt();
+        }
+      }
+    }
+    float iso(0.);
+    if (charged_only){
+      iso = iso_ch;
+    } else {
+      iso = iso_ph + iso_nh;
+      iso -= 0.5*iso_pu;
+      if (iso>0) iso += iso_ch;
+      else iso = iso_ch;
+    }
+    iso = iso/ptcl->pt();
+
+    return iso;
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
