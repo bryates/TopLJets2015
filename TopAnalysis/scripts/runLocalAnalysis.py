d7cbda7aa83d7d52cc1470325ba739bf9a59733f
Pedro Silva <psilva@cern.ch>
2016-04-27 11:49:44 +0200

    adding method for single jobs, removing flavour splitting for the moment

--- TopAnalysis/scripts/runLocalAnalysis.py
+++ TopAnalysis/scripts/runLocalAnalysis.py
@@ -27,18 +27,18 @@ def main():
     #configuration
     usage = 'usage: %prog [options]'
     parser = optparse.OptionParser(usage)
-    parser.add_option('-m', '--method',      dest='method',      help='method to run',                              default='TOP-16-006::RunTop16006',  type='string')
-    parser.add_option('-i', '--in',          dest='input',       help='input directory with files or single file',  default=None,       type='string')
-    parser.add_option('-o', '--out',         dest='output',      help='output directory (or file if single file to process)',  default='analysis', type='string')
-    parser.add_option(      '--only',        dest='only',        help='csv list of samples to process',             default=None,       type='string')
-    parser.add_option(      '--runSysts',    dest='runSysts',    help='run systematics',                            default=False,      action='store_true')
-    parser.add_option(      '--cache',       dest='cache',       help='use this cache file',                        default='data/genweights.pck', type='string')
-    parser.add_option(      '--flav',        dest='flav',        help='split according to heavy flavour content',   default=0,          type=int)
-    parser.add_option(      '--ch',          dest='channel',     help='channel',                                    default=13,         type=int)
-    parser.add_option(      '--charge',      dest='charge',      help='charge',                                     default=0,          type=int)
-    parser.add_option(      '--tag',         dest='tag',         help='normalize from this tag',                    default=None,       type='string')
-    parser.add_option('-q', '--queue',       dest='queue',       help='submit to this queue',                       default='local',    type='string')
-    parser.add_option('-n', '--njobs',       dest='njobs',       help='# jobs to run in parallel',                                default=0,    type='int')
+    parser.add_option('-m', '--method',      dest='method',      help='method to run [%default]',                   default='TOP-16-006::RunTop16006',  type='string')
+    parser.add_option('-i', '--in',          dest='input',       help='input directory with files or single file [%default]',  default=None,       type='string')
+    parser.add_option('-o', '--out',         dest='output',      help='output directory (or file if single file to process)  [%default]',  default='analysis', type='string')
+    parser.add_option(      '--only',        dest='only',        help='csv list of samples to process  [%default]',             default=None,       type='string')
+    parser.add_option(      '--runSysts',    dest='runSysts',    help='run systematics  [%default]',                            default=False,      action='store_true')
+    parser.add_option(      '--cache',       dest='cache',       help='use this cache file  [%default]',                        default='data/genweights.pck', type='string')
+    parser.add_option(      '--flav',        dest='flav',        help='split according to heavy flavour content  [%default]',   default=0,          type=int)
+    parser.add_option(      '--ch',          dest='channel',     help='channel  [%default]',                                    default=13,         type=int)
+    parser.add_option(      '--charge',      dest='charge',      help='charge  [%default]',                                     default=0,          type=int)
+    parser.add_option(      '--tag',         dest='tag',         help='normalize from this tag  [%default]',                    default=None,       type='string')
+    parser.add_option('-q', '--queue',       dest='queue',       help='submit to this queue  [%default]',                       default='local',    type='string')
+    parser.add_option('-n', '--njobs',       dest='njobs',       help='# jobs to run in parallel  [%default]',                                default=0,    type='int')
     (opt, args) = parser.parse_args()
 
     src,method=opt.method.split('::')
@@ -77,7 +77,7 @@ def main():
             if opt.tag in genWgts:
                 wgtH=genWgts[opt.tag]
         print inF,outF,opt.channel,opt.charge,wgtH,opt.flav,opt.runSysts
-        task_list.append( (inF,outF,opt.channel,opt.charge,wgtH,opt.flav,opt.runSysts) )
+        task_list.append( (method,inF,outF,opt.channel,opt.charge,wgtH,opt.flav,opt.runSysts) )
     else:
 
         inputTags=getEOSlslist(directory=opt.input,prepend='')
@@ -99,12 +99,12 @@ def main():
             for ifile in xrange(0,len(input_list)):
                 inF=input_list[ifile]
                 outF=os.path.join(opt.output,'%s_%d.root' %(tag,ifile))
-                doFlavourSplitting=True if ('MC13TeV_WJets' in tag or 'MC13TeV_DY50toInf' in tag) else False
-                if doFlavourSplitting:
-                    for flav in [0,1,4,5]:
-                        task_list.append( (method,inF,outF,opt.channel,opt.charge,wgtH,flav,opt.runSysts) )
-                else:
-                    task_list.append( (method,inF,outF,opt.channel,opt.charge,wgtH,0,opt.runSysts) )
+                #doFlavourSplitting=True if ('MC13TeV_WJets' in tag or 'MC13TeV_DY50toInf' in tag) else False
+                #if doFlavourSplitting:
+                #    for flav in [0,1,4,5]:
+                #        task_list.append( (method,inF,outF,opt.channel,opt.charge,wgtH,flav,opt.runSysts) )
+                #else:
+                task_list.append( (method,inF,outF,opt.channel,opt.charge,wgtH,0,opt.runSysts) )
 
     #run the analysis jobs
     if opt.queue=='local':
