30029fb44c0cce6666806e9dc49651ed5aef1ab1
Brent <brent.yates@email.ucr.edu>
2018-07-03 15:28:34 +0200

    GenWeights in CRAB 	modified:   test/runWeightCreationLoop.py

--- TopAnalysis/test/runWeightCreationLoop.py
+++ TopAnalysis/test/runWeightCreationLoop.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
+from WMCore.Configuration import Configuration
 import os
 
 #TUNES=[ ('up','BL',0.955), ('cuetp8m2t4','BL',0.855), ('down','BL',0.955) ]
 #TUNES=[ ('sup','BL',0.865), ('scentral','BL',0.854), ('sdown','BL',0.845), ('cuetp8m2t4','BL',0.855) ]
 #TUNES=[ ('scentral','BL',0.825), ('sdown','BL',0.655) ]
 #TUNES=[ ('sdown','BL',0.655), ('up','BL',1.055),  ('uup','BL',1.000), ('uuup','BL',0.975), ('central','BL',0.955), ('ccentral','BL',0.900), ('cccentral','BL',0.875), ('cuetp8m2t4','BL',0.855), ('scentral','BL',0.825), ('ddown','BL',0.800), ('dddown','BL',0.775), ('down','BL',0.755)]
-TUNES=[ ('1025','BL',1.025),  ('1075','BL',1.075), ('675','BL',0.675), ('625','BL',0.625), ('sdown','BL',0.655), ('up','BL',1.055),  ('uup','BL',1.000), ('uuup','BL',0.975), ('central','BL',0.955), ('ccentral','BL',0.900), ('cccentral','BL',0.875), ('cuetp8m2t4','BL',0.855), ('scentral','BL',0.825), ('ddown','BL',0.800), ('dddown','BL',0.775), ('down','BL',0.755) ]
+TUNES=[ ('1025','BL',1.025),  ('1075','BL',1.075), ('675','BL',0.675), ('625','BL',0.625), ('sdown','BL',0.655), ('up','BL',1.055),  ('uup','BL',1.000), ('uuup','BL',0.975), ('central','BL',0.955), ('ccentral','BL',0.900), ('cccentral','BL',0.875), ('scentral','BL',0.825), ('ddown','BL',0.800), ('dddown','BL',0.775), ('down','BL',0.755) ]
+#TUNES=[ ('1025','BL',1.025),  ('1075','BL',1.075), ('675','BL',0.675), ('625','BL',0.625), ('sdown','BL',0.655), ('up','BL',1.055),  ('uup','BL',1.000), ('uuup','BL',0.975), ('central','BL',0.955), ('ccentral','BL',0.900), ('cccentral','BL',0.875), ('cuetp8m2t4','BL',0.855), ('scentral','BL',0.825), ('ddown','BL',0.800), ('dddown','BL',0.775), ('down','BL',0.755) ]
 #TUNES=[ ('sup','BL',0.900), ('scentral','BL',0.875), ('cuetp8m2t4','BL',0.855), ('sdown','BL',0.825)]
 #TUNES=[ ('up','BL',1.055), ('central','BL',0.955), ('cuetp8m2t4','BL',0.855), ('down','BL',0.755)]
 #TUNES=[ ('up','BL',1.079), ('central','BL',0.8949), ('cuetp8m2t4','BL',0.855), ('down','BL',0.6981)]
@@ -22,22 +24,57 @@ def main():
     #run the analyzer
     for tag,frag,param in TUNES:
         #os.system('cmsRun %s maxEvents=%d frag=%s param=%f outputFile=xb_%s.root'%(CFG,MAXEVENTS,frag,param,tag))
-        target = '%s/src/TopLJets2015/TopAnalysis/%s' % (cmsswBase,tag)
-        condorFile = open(target,'w')
-        condorFile.write('universe              = vanilla\n')
-        condorFile.write('executable            = condor/cond_frag.sh\n')
-        condorFile.write('arguments             = $(ClusterID) $(ProcId) %s %s %s %s\n' % (tag, MAXEVENTS, frag, param))
-        condorFile.write('output                = condor/log/%s_$(ProcId).out\n' % tag)
-        condorFile.write('error                 = condor/log/%s_$(ProcId).err\n' % tag)
-        condorFile.write('log                   = condor/log/%s.log\n' % tag)
-        condorFile.write('Rank                  = Memory >= 64\n')
-        condorFile.write('Request_Memory        = 32 Mb\n')
-        condorFile.write('+JobFlavour           = "workday"\n')
-        condorFile.write('Should_Transfer_Files = NO\n')
-        condorFile.write('queue')
-        condorFile.close()
-        os.system('condor_submit %s -batch-name %s' % (target,tag))
-        os.system('rm %s' % (tag))
+        #target = '%s/src/TopLJets2015/TopAnalysis/%s' % (cmsswBase,tag)
+        #condorFile = open(target,'w')
+        #condorFile.write('universe              = vanilla\n')
+        #condorFile.write('executable            = condor/cond_frag.sh\n')
+        #condorFile.write('arguments             = $(ClusterID) $(ProcId) %s %s %s %s\n' % (tag, MAXEVENTS, frag, param))
+        #condorFile.write('output                = condor/log/%s_$(ProcId).out\n' % tag)
+        #condorFile.write('error                 = condor/log/%s_$(ProcId).err\n' % tag)
+        #condorFile.write('log                   = condor/log/%s.log\n' % tag)
+        #condorFile.write('Rank                  = Memory >= 64\n')
+        #condorFile.write('Request_Memory        = 32 Mb\n')
+        #condorFile.write('+JobFlavour           = "workday"\n')
+        #condorFile.write('Should_Transfer_Files = NO\n')
+        #condorFile.write('queue')
+        #condorFile.close()
+        #os.system('condor_submit %s -batch-name %s' % (target,tag))
+        #os.system('rm %s' % (tag))
+
+        workDir='grid'
+        crabConfigFile=workDir+'/'+tag+'_cfg.py'
+        config_file=open(crabConfigFile,'w')
+        config_file.write('from WMCore.Configuration import Configuration\n')
+        config_file.write('import os\n')
+        config_file.write('config = Configuration()\n')
+        config_file.write('\n')
+        
+        config_file.write('config.section_("General")\n')
+        config_file.write('config.General.requestName = "%s"\n' % tag)
+        config_file.write('config.General.workArea = "grid"\n')
+        config_file.write('config.General.transferOutputs=True\n')
+        config_file.write('\n')
+        config_file.write('config.section_("JobType")\n')
+        config_file.write('config.JobType.pluginName = "Analysis"\n')
+        config_file.write('config.JobType.psetName = "/afs/cern.ch/user/b/byates/CMSSW_8_0_26/src/TopLJets2015/TopAnalysis/test/runBFragmentationAnalyzer_cfg.py"\n')
+        config_file.write('config.JobType.disableAutomaticOutputCollection = False\n')
+        config_file.write('config.JobType.pyCfgParams = [\'frag=%s\',\'param=%f\',\'maxEvents=%d\']' % (frag,param,MAXEVENTS))
+        config_file.write('\n')
+        config_file.write('\n')
+        config_file.write('config.section_("Data")\n')
+        config_file.write('config.Data.inputDataset = "/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM"\n')
+        config_file.write('config.Data.inputDBS = "global"\n')
+        config_file.write('config.Data.splitting = "FileBased"\n')
+        config_file.write('config.Data.unitsPerJob = 4\n')
+        config_file.write('config.Data.publication = True\n')
+        config_file.write('config.Data.ignoreLocality = False\n')
+        config_file.write('config.Data.outLFNDirBase = \'/store/group/phys_top/byates/bfrag/%s\'\n' % tag)
+        config_file.write('\n')
+        config_file.write('config.section_("Site")\n')
+        config_file.write('config.Site.storageSite = "T2_CH_CERN"')
+        config_file.close()
+
+        os.system('alias crab=\'/cvmfs/cms.cern.ch/crab3/crab-env-bootstrap.sh\' && crab submit -c %s' % crabConfigFile )
 
 if __name__ == "__main__":
     main()
